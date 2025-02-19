4647138e72f716baa1e081c4c4bd9b09d85f21d8
msobremisana <msobremisana@up.edu.ph>
2025-02-19 11:57:16 +0800

    feat: added storePassword function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-console.log(validatePassword("helloworld","hello"));
-console.log(validatePassword("helloworld","helloworld"));
-console.log(validatePassword("hello123","hello123"));
-console.log(validatePassword("HELLO123","HELLO123"));
-console.log(validatePassword("Hello123","Hello123"));
+//console.log(validatePassword("helloworld","hello"));
+//console.log(validatePassword("helloworld","helloworld"));
+//console.log(validatePassword("hello123","hello123"));
+//console.log(validatePassword("HELLO123","HELLO123"));
+//console.log(validatePassword("Hello123","Hello123"));
 
-
+console.log(storePassword("John", "Pass1234", "Pass1234"));
+console.log(storePassword("John", "Pass123", "Pass123"));
 
 function validatePassword(str1,str2){
 
@@ -51,4 +52,21 @@ function reversePassword(password){
     for(let i = password.length-1; i >= 0; i--){
         newString = newString + password[i]
     }
+
+    return newString;
+}
+
+function storePassword(name,password1,password2){
+    var pass = "";
+    const user = {
+        name: name
+    }
+    if(validatePassword(password1,password2)){ // store if password is valid
+        pass = reversePassword(password1);
+        user.newpassword = pass;
+    }else{
+        user.newpassword = password1;
+    }
+
+    return user;
 }
